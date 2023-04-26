4443e29ed9527300aa41cde1b9472dcdc843ac8c
Ivlev Semyon <75061655+IvlevSemyon@users.noreply.github.com>
2023-04-26 20:05:06 +0300

    Add files via upload

---
+++ 002_DZ_Inheritance/ClassRoom.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_DZ_Inheritance
+{
+    internal class ClassRoom
+    {
+        Pupil[] pupils;
+
+        public ClassRoom( Pupil p1, Pupil p2, Pupil p3, Pupil p4 )
+        {
+            pupils = new Pupil[] { p1, p2, p3, p4 };
+        }
+
+        public ClassRoom( Pupil p1, Pupil p2, Pupil p3 )
+        {
+            pupils = new Pupil[] { p1, p2, p3 };
+        }
+
+        public ClassRoom( Pupil p1, Pupil p2 )
+        {
+            pupils = new Pupil[] { p1, p2 };
+        }
+
+        public void PrintInfoStudy()
+        {
+            Console.WriteLine("\nStudy: ");
+            foreach (var pupil in pupils)
+            {
+                pupil.Study();
+            }
+        }
+
+        public void PrintInfoWrite( )
+        {
+            Console.WriteLine( "\nWrite: " );
+            foreach ( var pupil in pupils )
+            {
+                pupil.Write();
+            }
+        }
+
+        public void PrintInfoRead( )
+        {
+            Console.WriteLine( "\nRead: " );
+            foreach ( var pupil in pupils )
+            {
+                pupil.Read();
+            }
+        }
+
+        public void PrintInfoRelax( )
+        {
+            Console.WriteLine( "\nRelax: " );
+            foreach ( var pupil in pupils )
+            {
+                pupil.Relax();
+            }
+        }
+    }
+}
---
+++ 002_DZ_Inheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_DZ_Inheritance
+{
+    internal class Program
+    {
+        public static void ConsolePrintPupilName()
+        {
+            Console.WriteLine("Pupil1 | Pupil2 | Pupil3 | Pupil4");
+        }
+        static void Main( string[] args )
+        {
+            ClassRoom cRoom = new ClassRoom( new ExcelentPupil(), new BadPupil(), new GoodPupil(), new ExcelentPupil() );
+
+            ConsolePrintPupilName();
+            cRoom.PrintInfoStudy();
+            cRoom.PrintInfoWrite();
+            cRoom.PrintInfoRead();
+            cRoom.PrintInfoRelax();
+
+            Console.ReadKey();
+        }
+    }
+}
